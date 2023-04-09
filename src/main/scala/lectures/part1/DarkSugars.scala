d3d351c8b29774945089b69396dd7aa535e10fee
Pedro Foresti Leão <forestileao@pop-os.localdomain>
2023-04-09 02:51:10 -0300

    Practiced Dark Syntax Sugars

--- src/main/scala/lectures/part1/DarkSugars.scala
+++ src/main/scala/lectures/part1/DarkSugars.scala
@@ -1,5 +1,99 @@
 package lectures.part1
 
-object DarkSugars {
+import scala.util.Try
 
+object DarkSugars extends App {
+
+  // #1 single arg methods
+  def singleArgMethod(arg: Int): String = s"$arg little ducks"
+
+  val description = singleArgMethod {
+    // write some code
+    43 // return the result at the end
+  }
+
+  val tryInstance = Try { // java's try {...}
+    throw new RuntimeException
+  }
+
+  List(1, 2, 3, 4).map { x =>
+    x + 1
+  }
+
+  // #2 single abstract method
+  trait Action {
+    def act(x: Int): Int
+  }
+
+  val instance: Action = new Action {
+    override def act(x: Int): Int = x + 1
+  }
+
+  val funkyInstance: Action = (x: Int) => x + 1 // magic
+
+  val simpleThread = new Thread(new Runnable {
+    override def run(): Unit = println("Hello, Pedro!")
+  })
+
+  val sweeterThread = new Thread(() => println("sweet pedro"))
+
+  abstract class AnAbstractType {
+    def implemented: Int = 23
+
+    def f(a: Int): Unit
+  }
+
+  val anAbstractInstance: AnAbstractType = (a: Int) => println(a)
+
+  // #3 the :: and #:: methods are special
+
+  val prependedList = 2 :: List(3, 4)
+  // List(3, 4).::(2) ???
+
+  // answer: scala spec => last char decides associativity of method
+  1 :: 2 :: 3 :: List(4, 5)
+  // ==
+  List(4 ,5).::(3).::(2).::(1)
+
+  class MyStream[T] {
+    // note the ':' after arrow, it tells to the compile that the last expression will be evaluated first
+    def -->:(value: T): MyStream[T] = this // actual implementation here
+  }
+
+  // new MyStream will be evaluated first
+  val myStream = 1 -->: 2 -->: 3 -->: new MyStream[Int]
+
+  // syntax sugar #4: multi-word method naming
+  class ScoobyDoo(name: String) {
+    def `e a perua falou`(gossip: String): Unit = println(s"E a perua falou $gossip")
+  }
+
+  val fred = new ScoobyDoo("Fred")
+  fred `e a perua falou` "\"Ta afim?\" e eu disse \"agora não\""
+
+  // #5: Infix types
+  class Composite[A, B]
+  val composite: Composite[Int, String] = ???
+  val otherComposite: Int Composite String = ???
+
+  class -->[A, B]
+  val towards: Int --> String = ???
+
+  // # 6: update() is very special, much like apply()
+  val anArray = Array(1, 2, 3)
+  anArray(2) = 7 // rewritten to anArray.update(2, 7)
+  // used in mutable collections
+  // remember apply() and update()
+
+  // #7: setters for mutable containers
+  class Mutable {
+    private var internalMember: Int = 0 // private for OO encapsulation
+    def member = internalMember // "getter"
+    def member_=(value: Int):Unit =
+      internalMember = value // "setter"
+  }
+
+  val mutableContainer = new Mutable
+
+  mutableContainer.member = 42 // rewritten as  mutableContainer.member_=(42)
 }
--- src/main/scala/lectures/part1/Recap.scala
+++ src/main/scala/lectures/part1/Recap.scala
@@ -1,5 +1,113 @@
 package lectures.part1
 
-class Recap {
+import scala.annotation.tailrec
 
+object Recap extends App {
+  val condition: Boolean = false
+  val conditionedVal = if condition then 43 else 65
+
+  // instructions vs expressions
+
+  // compiler infers types for us
+  val codeBlock = {
+    if condition then 64 else 53
+  }
+
+  // Unit = void
+  val theUnit = println("Hello, Scala")
+
+  // functions
+  def function(x: Int): Int = x + 1
+
+  // recursion: stack and tail
+  @tailrec
+  def factorial(n: Int, acc: Int): Int =
+    if n <= 0 then acc else factorial(n - 1, acc)
+
+  // object-oriented programming
+  class Animal
+
+  class Dog extends Animal
+
+  val dog: Animal = new Dog
+
+  trait Carnivore {
+    def eat(a: Animal): Unit
+  }
+
+  class Crocodile extends Animal with Carnivore {
+    override def eat(a: Animal): Unit = println("Crunch!")
+  }
+
+  // val notation
+  val croc = Crocodile()
+  croc.eat(dog)
+  croc eat dog
+
+  val newCroc = new Crocodile {
+    override def eat(a: Animal): Unit = println("Roar!")
+  }
+
+  // generics
+  abstract class MyList[+A] // variance and variance problems
+
+  // singletons and companions
+  object MyList
+
+  // case classes
+  case class Person(name: String, age: Int)
+
+  // exceptions try/catch/finally
+
+  val throwsException = throw new RuntimeException // nothing
+  val potencialFailure = try {
+    throw new RuntimeException
+  } catch {
+    case e: Exception => "I caught an exception!"
+  } finally {
+    println("Some logs")
+  }
+
+  // packaging and imports
+
+  // functional programming
+  var incrementer = new Function[Int, Int] {
+    override def apply(v1: Int): Int = v1 + 1
+  }
+
+  incrementer(1)
+
+  val anonymousIncrementer = (x: Int) => x + 1
+  List(1, 2, 3).map(anonymousIncrementer) // HOF
+  // map, flatMap, filter
+
+  // for-comprehension
+  val pars = for {
+    num <- List(1, 2, 3) // if condition
+    char <- List('a', 'b', 'c')
+  } yield num + "-" + char
+
+  // Scala collections:  Seqs, Arrays, Lists, Vectors, Maps, Tuples
+  val map = Map(
+    "Daniel" -> 789,
+    "Pastel" -> 555
+  )
+
+  // "collections": Options and tries
+  val anOption = Some(2)
+
+  // Pattern Matching
+  val x = 2
+  val order = x match
+    case 1 => "first"
+    case 2 => "second"
+    case 3 => "third"
+    case _ => x + "th"
+
+  val bob = Person("Bob", 22)
+
+  val greeting = bob match
+    case Person(n, _) => s"Hello, $n"
+
+  // all the patterns
 }
